8461598f04b8729e6c1bb8fedec106383fab8dd2
hikobe8 <hikoeb8@163.com>
2016-10-31 23:39:50 +0800

    chapter String

---
+++ demo3.php
@@ -0,0 +1,13 @@
+<?php
+	$name = 'steve';
+	$age = 24;
+	$adult = true;
+	
+	var_dump($name);
+	echo "<br/>";
+	var_dump($age);
+	echo "<br/>";
+	var_dump($adult);
+	echo "<br/>";
+	$result = pow(2, 31);
+	echo $result;
---
+++ demo4.php
@@ -0,0 +1,8 @@
+<?php
+	$s1 = 'this is a  \n \' simple\' string';
+	echo $s1;
+	echo "<br/>";
+	$name = 'kobe';
+	$s2 = "this is $name";
+	echo "<br/>";
+	echo $s2;
